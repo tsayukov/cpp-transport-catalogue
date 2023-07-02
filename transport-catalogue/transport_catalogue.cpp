17ed268762e073718a737e89a7a0deb1b506c8bc
Pasha Tsayukov <p.tsayukov@gmail.com>
2023-07-03 00:15:29 +0300

    Simplified and split many methods; added new AddBus

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,103 +1,80 @@
 #include "transport_catalogue.h"
 
-#include <algorithm>
-#include <cassert>
+#include "kahan_algorithm.h"
+
 #include <unordered_set>
 
 namespace transport_catalogue {
 
-void TransportCatalogue::AddStop(const Stop& stop) {
-    Stop stop_copy = stop;
-    AddStop(std::move(stop_copy));
-}
-
-void TransportCatalogue::AddStop(Stop&& stop) {
+void TransportCatalogue::AddStop(Stop stop) {
     stops_.push_back(std::move(stop));
     StopPtr stop_ptr = &stops_.back();
     stop_indices_.emplace(stop_ptr->name, stop_ptr);
 }
 
-void TransportCatalogue::AddBus(const Bus& bus) {
-    Bus bus_copy = bus;
-    AddBus(std::move(bus_copy));
-}
-
-void TransportCatalogue::AddBus(Bus&& bus) {
+void TransportCatalogue::AddBus(Bus bus) {
     buses_.push_back(std::move(bus));
     BusPtr bus_ptr = &buses_.back();
     bus_indices_.emplace(bus_ptr->name, bus_ptr);
 
     for (StopPtr stop_ptr : bus_ptr->stops) {
-        auto iter = stop_statistics_.find(stop_ptr);
-        if (iter != stop_statistics_.end()) {
-            iter->second.buses.emplace_back(bus_ptr);
-            iter->second.is_buses_unique_and_ordered = false;
+        StopInfoStorage* stop_info_storage_ptr = nullptr;
+        if (auto stop_info_iter = stop_infos_.find(stop_ptr); stop_info_iter != stop_infos_.end()) {
+            stop_info_storage_ptr = &stop_info_iter->second;
         } else {
-            StopInfo stop_info{};
-            stop_info.buses.emplace_back(bus_ptr);
-            stop_info.is_buses_unique_and_ordered = false;
-            stop_statistics_.emplace(stop_ptr, std::move(stop_info));
+            auto [iter, _] = stop_infos_.emplace(stop_ptr, StopInfoStorage{});
+            stop_info_storage_ptr = &iter->second;
         }
+        stop_info_storage_ptr->stop_info.buses.emplace_back(bus_ptr);
+        stop_info_storage_ptr->is_buses_sorted_and_unique = false;
     }
 }
 
-void TransportCatalogue::SetDistance(std::string_view stop_name_from, std::string_view stop_name_to,
-                                     geo::Meter distance) {
-    auto iter_from = stop_indices_.find(stop_name_from);
-    auto iter_to = stop_indices_.find(stop_name_to);
-    if (iter_from == stop_indices_.end() || iter_to == stop_indices_.end()) {
-        return;
+void TransportCatalogue::SetDistanceBetweenStops(std::string_view from, std::string_view to, geo::Meter distance) {
+    auto stop_ptr_from = FindStopBy(from);
+    auto stop_ptr_to = FindStopBy(to);
+    if (!stop_ptr_from.has_value() || !stop_ptr_to.has_value()) {
+        using namespace std::string_literals;
+        throw std::domain_error(
+                "Error occurs during setting a distance between stops: one or both of the stops does not exist"s);
     }
-
-    StopPtr stop_from_ptr = iter_from->second;
-    StopPtr stop_to_ptr = iter_to->second;
-    distances_.emplace(std::make_pair(stop_from_ptr, stop_to_ptr), distance);
+    distances_.emplace(std::make_pair(stop_ptr_from.value(), stop_ptr_to.value()), distance);
 }
 
-std::optional<geo::Meter> TransportCatalogue::GetDistance(std::string_view stop_name_from,
-                                                          std::string_view stop_name_to) const {
-    auto iter_from = stop_indices_.find(stop_name_from);
-    auto iter_to = stop_indices_.find(stop_name_to);
-    if (iter_from == stop_indices_.end() || iter_to == stop_indices_.end()) {
+std::optional<geo::Meter> TransportCatalogue::GetDistanceBetweenStops(std::string_view from, std::string_view to) const {
+    auto stop_ptr_from = FindStopBy(from);
+    auto stop_ptr_to = FindStopBy(to);
+    if (!stop_ptr_from.has_value() || !stop_ptr_to.has_value()) {
         return std::nullopt;
     }
-
-    const Stop& stop_from = *iter_from->second;
-    const Stop& stop_to = *iter_to->second;
-    return GetDistance(stop_from, stop_to);
+    return GetDistance(*stop_ptr_from.value(), *stop_ptr_to.value());
 }
 
 std::optional<BusPtr> TransportCatalogue::FindBusBy(std::string_view bus_name) const {
-    if (auto iter = bus_indices_.find(bus_name); iter == bus_indices_.end()) {
-        return std::nullopt;
-    } else {
-        BusPtr bus = iter->second;
-        return bus;
+    if (auto iter = bus_indices_.find(bus_name); iter != bus_indices_.end()) {
+        BusPtr bus_ptr = iter->second;
+        return bus_ptr;
     }
+    return std::nullopt;
 }
 
 std::optional<StopPtr> TransportCatalogue::FindStopBy(std::string_view stop_name) const {
-    if (auto iter = stop_indices_.find(stop_name); iter == stop_indices_.end()) {
-        return std::nullopt;
-    } else {
-        StopPtr stop = iter->second;
-        return stop;
+    if (auto iter = stop_indices_.find(stop_name); iter != stop_indices_.end()) {
+        StopPtr stop_ptr = iter->second;
+        return stop_ptr;
     }
+    return std::nullopt;
 }
 
-TransportCatalogue::StopConstIters TransportCatalogue::GetAllStops() const noexcept {
-    return { stops_.cbegin(), stops_.cend() };
+TransportCatalogue::StopRange TransportCatalogue::GetAllStops() const noexcept {
+    return ranges::AsConstRange(stops_);
 }
 
-TransportCatalogue::BusConstIters TransportCatalogue::GetAllBuses() const noexcept {
-    return { buses_.cbegin(), buses_.cend() };
+TransportCatalogue::BusRange TransportCatalogue::GetAllBuses() const noexcept {
+    return ranges::AsConstRange(buses_);
 }
 
 // Statistics
-
-TransportCatalogue::StopInfo::StopInfo(std::vector<BusPtr> buses)
-        : buses(std::move(buses)) {
-}
 
 std::optional<const TransportCatalogue::StopInfo*> TransportCatalogue::GetStopInfo(std::string_view stop_name) const {
     const auto stop = FindStopBy(stop_name);
@@ -105,25 +82,14 @@ std::optional<const TransportCatalogue::StopInfo*> TransportCatalogue::GetStopIn
         return std::nullopt;
     }
 
-    auto iter_stop_stat = stop_statistics_.find(stop.value());
-    if (iter_stop_stat == stop_statistics_.end()) {
-        const auto& stop_info = stop_statistics_.emplace(stop.value(), StopInfo{}).first->second;
-        return &stop_info;
+    if (auto stop_info_iter = stop_infos_.find(stop.value()); stop_info_iter != stop_infos_.end()) {
+        auto& stop_info_storage = stop_info_iter->second;
+        return &PrepareStopInfo(stop_info_storage);
     }
 
-    StopInfo& stop_info = iter_stop_stat->second;
-    auto& buses = stop_info.buses;
-    if (!stop_info.is_buses_unique_and_ordered) {
-        std::sort(buses.begin(), buses.end(), [](const Bus* lhs, const Bus* rhs) noexcept {
-                return lhs->name < rhs->name;
-        });
-        auto begin_to_remove = std::unique(buses.begin(), buses.end(), [](const Bus* lhs, const Bus* rhs) noexcept {
-                return lhs->name == rhs->name;
-        });
-        buses.erase(begin_to_remove, buses.end());
-        stop_info.is_buses_unique_and_ordered = true;
-    }
-    return &stop_info;
+    auto [stop_info_iter, _] = stop_infos_.emplace(stop.value(), StopInfoStorage{});
+    const auto& stop_info_storage = stop_info_iter->second;
+    return &stop_info_storage.stop_info;
 }
 
 std::optional<const TransportCatalogue::BusInfo*> TransportCatalogue::GetBusInfo(std::string_view bus_name) const {
@@ -132,72 +98,71 @@ std::optional<const TransportCatalogue::BusInfo*> TransportCatalogue::GetBusInfo
         return std::nullopt;
     }
 
-    auto iter_bus_stat = bus_statistics_.find(bus.value());
-    const BusInfo& bus_info = (iter_bus_stat == bus_statistics_.end())
-                            ? ComputeBusStatistics(*bus.value())
-                            : iter_bus_stat->second;
-    return &bus_info;
+    if (auto bus_stat_iter = bus_infos_.find(bus.value()); bus_stat_iter != bus_infos_.end()) {
+        return &bus_stat_iter->second;
+    }
+    return &ComputeBusInfo(*bus.value());
 }
 
-namespace details {
-
-/// Kahan summation algorithm
-/// source: https://en.wikipedia.org/wiki/Kahan_summation_algorithm
-struct KahanFloatingPointAddition {
-    double acc = 0.0;
-    double compensation = 0.0;
-
-    [[nodiscard]] double operator()(double value) noexcept {
-        double y = value - compensation;
-        double t = acc + y;
-        compensation = (t - acc) - y;
-        acc = t;
-        return acc;
+const TransportCatalogue::StopInfo& TransportCatalogue::PrepareStopInfo(StopInfoStorage& stop_info_storage) {
+    auto& buses = stop_info_storage.stop_info.buses;
+    if (!stop_info_storage.is_buses_sorted_and_unique) {
+        std::sort(buses.begin(), buses.end(), [](BusPtr lhs, BusPtr rhs) noexcept {
+            return lhs->name < rhs->name;
+        });
+        auto begin_to_remove = std::unique(buses.begin(), buses.end(), [](BusPtr lhs, BusPtr rhs) noexcept {
+            return lhs->name == rhs->name;
+        });
+        buses.erase(begin_to_remove, buses.end());
+        stop_info_storage.is_buses_sorted_and_unique = true;
     }
-};
-
+    return stop_info_storage.stop_info;
 }
 
-const TransportCatalogue::BusInfo& TransportCatalogue::ComputeBusStatistics(const Bus& bus) const {
-    auto [iter, _] = bus_statistics_.emplace(&bus, BusInfo{});
-    BusInfo& bus_info = iter->second;
-    if (bus.stops.empty()) {
-        return bus_info;
-    }
+const TransportCatalogue::BusInfo& TransportCatalogue::ComputeBusInfo(const Bus& bus) const {
+    auto [bus_info_iter, _] = bus_infos_.emplace(&bus, BusInfo{});
+    BusInfo& bus_info = bus_info_iter->second;
 
+    const auto& stops = bus.stops;
     std::unordered_set<const Stop*> unique_stops;
-    unique_stops.reserve(bus.stops.size());
-
-    details::KahanFloatingPointAddition double_sum_route_length;
-    details::KahanFloatingPointAddition double_sum_geo_length;
-
-    const auto stops_size = bus.stops.size();
-    for (std::size_t i = 0; i < stops_size - 1; ++i) {
-        unique_stops.emplace(bus.stops[i]);
-
-        auto distance = GetDistance(*bus.stops[i], *bus.stops[i + 1]);
-        bus_info.route_length = double_sum_route_length(distance);
-
-        auto geo_distance = GetGeoDistance(*bus.stops[i], *bus.stops[i + 1]);
-        bus_info.geo_length = double_sum_geo_length(geo_distance);
+    unique_stops.reserve(stops.size());
+    for (StopPtr stop_ptr : stops) {
+        unique_stops.emplace(stop_ptr);
     }
-    unique_stops.emplace(bus.stops.back());
-
-    if (bus.route_type == Bus::RouteType::Half) {
-        bus_info.geo_length *= 2.0;
-
-        for (std::size_t i = stops_size - 1; i > 0; --i) {
-            auto distance = GetDistance(*bus.stops[i], *bus.stops[i - 1]);
-            bus_info.route_length = double_sum_route_length(distance);
-        }
-    }
-
-    bus_info.stops_count = (bus.route_type == Bus::RouteType::Full) ? bus.stops.size() : bus.stops.size() * 2 - 1;
     bus_info.unique_stops_count = unique_stops.size();
+    bus_info.length = ComputeFullRouteDistance(bus);
+    bus_info.stops_count = (bus.route_type == Bus::RouteType::Full) ? stops.size() : (stops.size() * 2 - 1);
     return bus_info;
 }
 
 // Distance
+
+TransportCatalogue::BusInfo::Length TransportCatalogue::ComputeFullRouteDistance(const Bus& bus) const {
+    using ranges::Zip;
+    using ranges::Drop;
+    using ranges::Reverse;
+
+    BusInfo::Length length;
+    kahan_algorithm::Summation<geo::Meter> sum_route_length;
+    kahan_algorithm::Summation<geo::Meter> sum_geo_length;
+
+    const auto& stops = bus.stops;
+    for (const auto [stop_ptr_from, stop_ptr_to] : Zip(stops, Drop(stops, 1))) {
+        sum_route_length += GetDistance(*stop_ptr_from, *stop_ptr_to).value_or(geo::Meter{0});
+        sum_geo_length += GetGeoDistance(*stop_ptr_from, *stop_ptr_to);
+    }
+    length.geo = sum_geo_length.Get();
+
+    if (bus.route_type == Bus::RouteType::Half) {
+        length.geo *= 2;
+        for (const auto [stop_ptr_from, stop_ptr_to] : Zip(Reverse(stops), Drop(Reverse(stops), 1))) {
+            sum_route_length += GetDistance(*stop_ptr_from, *stop_ptr_to).value_or(geo::Meter{0});
+        }
+    }
+    length.route = sum_route_length.Get();
+
+    return length;
+}
 
 std::size_t TransportCatalogue::StopPtrPairHasher::operator()(std::pair<StopPtr, StopPtr> stops) const noexcept {
     static std::hash<StopPtr> stop_hash;
@@ -205,19 +170,16 @@ std::size_t TransportCatalogue::StopPtrPairHasher::operator()(std::pair<StopPtr,
     return stop_hash(stops.first) + prime_num * stop_hash(stops.second);
 }
 
-geo::Meter TransportCatalogue::GetDistance(const Stop& from, const Stop& to) const {
-    auto iter = distances_.find({&from, &to});
-    if (iter != distances_.end()) {
-        return iter->second;
+std::optional<geo::Meter> TransportCatalogue::GetDistance(const Stop& from, const Stop& to) const {
+    geo::Meter distance;
+    if (auto from_to_iter = distances_.find({&from, &to}); from_to_iter != distances_.end()) {
+        distance = from_to_iter->second;
+    } else if (auto to_from_iter = distances_.find({&to, &from}); to_from_iter != distances_.end()) {
+        distance = to_from_iter->second;
+        distances_.emplace(std::make_pair(&from, &to), distance);
+    } else {
+        return std::nullopt;
     }
-
-    auto distance = geo::Meter(0);
-    iter = distances_.find({&to, &from});
-    assert(iter != distances_.end());
-
-    distance = iter->second;
-    distances_.emplace(std::make_pair(&from, &to), distance);
-
     return distance;
 }
 
--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -5,9 +5,12 @@
 
 #include "geo.h"
 #include "domain.h"
+#include "ranges.h"
 
+#include <algorithm>
 #include <deque>
 #include <optional>
+#include <string>
 #include <string_view>
 #include <unordered_map>
 #include <vector>
@@ -16,49 +19,45 @@ namespace transport_catalogue {
 
 class TransportCatalogue final {
 private:
-    using StopConstIter = typename std::deque<Stop>::const_iterator;
-    using StopConstIters = std::pair<StopConstIter, StopConstIter>;
+    using StopIterator = typename std::deque<Stop>::const_iterator;
+    using BusIterator = typename std::deque<Bus>::const_iterator;
 
-    using BusConstIter = typename std::deque<Bus>::const_iterator;
-    using BusConstIters = std::pair<BusConstIter, BusConstIter>;
 public:
-    void AddStop(const Stop& stop);
-    void AddStop(Stop&& stop);
+    void AddStop(Stop stop);
+    void AddBus(Bus bus);
 
-    void AddBus(const Bus& bus);
-    void AddBus(Bus&& bus);
+    template<typename StopContainer>
+    void AddBus(std::string name, const StopContainer& stop_names, Bus::RouteType route_type);
 
-    void SetDistance(std::string_view stop_name_from, std::string_view stop_name_to, geo::Meter distance);
-
-    [[nodiscard]] std::optional<geo::Meter> GetDistance(std::string_view stop_name_from,
-                                                        std::string_view stop_name_to) const;
+    void SetDistanceBetweenStops(std::string_view from, std::string_view to, geo::Meter distance);
+    [[nodiscard]] std::optional<geo::Meter> GetDistanceBetweenStops(std::string_view from, std::string_view to) const;
 
     [[nodiscard]] std::optional<StopPtr> FindStopBy(std::string_view stop_name) const;
     [[nodiscard]] std::optional<BusPtr> FindBusBy(std::string_view bus_name) const;
 
-    [[nodiscard]] StopConstIters GetAllStops() const noexcept;
-    [[nodiscard]] BusConstIters GetAllBuses() const noexcept;
+    using StopRange = ranges::ConstRange<StopIterator>;
+    using BusRange = ranges::ConstRange<BusIterator>;
+
+    [[nodiscard]] StopRange GetAllStops() const noexcept;
+    [[nodiscard]] BusRange GetAllBuses() const noexcept;
 
     // Statistics
 
     struct StopInfo {
-    public:
         std::vector<BusPtr> buses;
-
-        explicit StopInfo(std::vector<BusPtr> buses = {});
-
-    private:
-        friend TransportCatalogue;
-        bool is_buses_unique_and_ordered = false;
     };
 
     [[nodiscard]] std::optional<const StopInfo*> GetStopInfo(std::string_view stop_name) const;
 
     struct BusInfo {
+        struct Length {
+            geo::Meter route;
+            geo::Meter geo;
+        };
+
         unsigned int stops_count = 0;
         unsigned int unique_stops_count = 0;
-        geo::Meter route_length = geo::Meter(0.0);
-        geo::Meter geo_length = geo::Meter(0.0);
+        Length length;
     };
 
     [[nodiscard]] std::optional<const BusInfo*> GetBusInfo(std::string_view bus_name) const;
@@ -75,12 +74,21 @@ private:
 
     // Statistics
 
-    mutable std::unordered_map<StopPtr, StopInfo> stop_statistics_;
-    mutable std::unordered_map<BusPtr, BusInfo> bus_statistics_;
+    struct StopInfoStorage {
+        StopInfo stop_info;
+        bool is_buses_sorted_and_unique = false;
+    };
 
-    const BusInfo& ComputeBusStatistics(const Bus& bus) const;
+    mutable std::unordered_map<StopPtr, StopInfoStorage> stop_infos_;
+    mutable std::unordered_map<BusPtr, BusInfo> bus_infos_;
+
+    static const StopInfo& PrepareStopInfo(StopInfoStorage& stop_info_storage);
+
+    const BusInfo& ComputeBusInfo(const Bus& bus) const;
 
     // Distance
+
+    BusInfo::Length ComputeFullRouteDistance(const Bus& bus) const;
 
     struct StopPtrPairHasher {
         std::size_t operator()(std::pair<StopPtr, StopPtr> stops) const noexcept;
@@ -88,7 +96,24 @@ private:
 
     mutable std::unordered_map<std::pair<StopPtr, StopPtr>, geo::Meter, StopPtrPairHasher> distances_;
 
-    [[nodiscard]] geo::Meter GetDistance(const Stop& from, const Stop& to) const;
+    [[nodiscard]] std::optional<geo::Meter> GetDistance(const Stop& from, const Stop& to) const;
 };
+
+template<typename StopContainer>
+void TransportCatalogue::AddBus(std::string name, const StopContainer& stop_names, Bus::RouteType route_type) {
+    Bus bus;
+    bus.name = std::move(name);
+    bus.route_type = route_type;
+    bus.stops.reserve(stop_names.size());
+    for (const auto& stop_name : stop_names) {
+        if (auto stop = FindStopBy(stop_name)) {
+            bus.stops.push_back(stop.value());
+        } else {
+            using namespace std::string_literals;
+            throw std::domain_error("Error occurs during creation of a bus: there is no such stop in the database"s);
+        }
+    }
+    AddBus(std::move(bus));
+}
 
 } // namespace transport_catalogue
