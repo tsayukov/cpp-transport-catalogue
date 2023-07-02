51c2e4902efc27c392481701c5265abc74456bba
Pasha Tsayukov <p.tsayukov@gmail.com>
2023-07-03 00:35:09 +0300

    Now Handler works with a new parser/process/print system

--- transport-catalogue/request_handler.cpp
+++ transport-catalogue/request_handler.cpp
@@ -2,11 +2,49 @@
 
 #include <functional>
 
-namespace transport_catalogue::query {
+namespace transport_catalogue::queries {
 
-Handler::Handler(const TransportCatalogue& database, const renderer::MapRenderer& renderer) noexcept
+Handler::Handler(TransportCatalogue& database,
+                 renderer::MapRenderer& renderer,
+                 router::TransportRouter& router) noexcept
         : database_(database)
-        , renderer_(renderer) {
+        , renderer_(renderer)
+        , router_(router) {
+}
+
+// Transport Catalogue methods adapters
+
+void Handler::AddStop(Stop stop) {
+    database_.AddStop(std::move(stop));
+}
+
+void Handler::AddBus(Bus bus) {
+    database_.AddBus(std::move(bus));
+}
+
+void Handler::SetDistanceBetweenStops(std::string_view from, std::string_view to, geo::Meter distance) {
+    database_.SetDistanceBetweenStops(from, to, distance);
+}
+
+[[nodiscard]] std::optional<geo::Meter> Handler::GetDistanceBetweenStops(std::string_view from,
+                                                                         std::string_view to) const {
+    return database_.GetDistanceBetweenStops(from, to);
+}
+
+[[nodiscard]] std::optional<StopPtr> Handler::FindStopBy(std::string_view stop_name) const {
+    return database_.FindStopBy(stop_name);
+}
+
+[[nodiscard]] std::optional<BusPtr> Handler::FindBusBy(std::string_view bus_name) const {
+    return database_.FindBusBy(bus_name);
+}
+
+[[nodiscard]] TransportCatalogue::StopRange Handler::GetAllStops() const noexcept {
+    return database_.GetAllStops();
+}
+
+[[nodiscard]] TransportCatalogue::BusRange Handler::GetAllBuses() const noexcept {
+    return database_.GetAllBuses();
 }
 
 std::optional<const TransportCatalogue::BusInfo*> Handler::GetBusInfo(std::string_view bus_name) const {
@@ -17,9 +55,19 @@ std::optional<const TransportCatalogue::StopInfo*> Handler::GetStopInfo(std::str
     return database_.GetStopInfo(stop_name);
 }
 
-svg::Document Handler::RenderMap() const {
-    auto [bus_first, bus_last] = database_.GetAllBuses();
-    return renderer_.Render(bus_first, bus_last);
+// Map Renderer methods adapters
+
+void Handler::InitializeMapRenderer(renderer::Settings settings) {
+    renderer_.Initialize(std::move(settings));
 }
 
-} // namespace transport_catalogue::query
+svg::Document Handler::RenderMap() const {
+    const auto bus_range = database_.GetAllBuses();
+    return renderer_.Render(bus_range.begin(), bus_range.end());
+}
+
+// Transport Router methods adapters
+
+// todo impl
+
+} // namespace transport_catalogue::queries
--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -3,29 +3,76 @@
 
 #pragma once
 
-#include "svg.h"
-#include "map_renderer.h"
+#include "geo.h"
 #include "domain.h"
 #include "transport_catalogue.h"
+#include "map_renderer.h"
+#include "transport_router.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+#include "json_reader.h"
 
+#include <iostream>
 #include <optional>
 #include <string_view>
 
-namespace transport_catalogue::query {
+namespace transport_catalogue::queries {
 
 class Handler final {
 public:
-    explicit Handler(const TransportCatalogue& database, const renderer::MapRenderer& renderer_) noexcept;
+    explicit Handler(TransportCatalogue& database,
+                     renderer::MapRenderer& renderer,
+                     router::TransportRouter& router) noexcept;
+
+    // Queries process methods adapters
+
+    template<typename From, typename Into>
+    void ProcessQueries(From from, std::istream& input, Into into, std::ostream& output);
+
+    // Transport Catalogue methods adapters
+
+    void AddStop(Stop stop);
+    void AddBus(Bus bus);
+
+    template<typename StopContainer>
+    void AddBus(std::string name, const StopContainer& stop_names, Bus::RouteType route_type);
+
+    void SetDistanceBetweenStops(std::string_view from, std::string_view to, geo::Meter distance);
+    [[nodiscard]] std::optional<geo::Meter> GetDistanceBetweenStops(std::string_view from, std::string_view to) const;
+
+    [[nodiscard]] std::optional<StopPtr> FindStopBy(std::string_view stop_name) const;
+    [[nodiscard]] std::optional<BusPtr> FindBusBy(std::string_view bus_name) const;
+
+    [[nodiscard]] TransportCatalogue::StopRange GetAllStops() const noexcept;
+    [[nodiscard]] TransportCatalogue::BusRange GetAllBuses() const noexcept;
 
     [[nodiscard]] std::optional<const TransportCatalogue::BusInfo*> GetBusInfo(std::string_view bus_name) const;
-
     [[nodiscard]] std::optional<const TransportCatalogue::StopInfo*> GetStopInfo(std::string_view stop_name) const;
+
+    // Map Renderer methods adapters
+
+    void InitializeMapRenderer(renderer::Settings settings);
 
     [[nodiscard]] svg::Document RenderMap() const;
 
+    // Transport Router methods adapters
+
+    // todo impl
+
 private:
-    const TransportCatalogue& database_;
-    const renderer::MapRenderer& renderer_;
+    TransportCatalogue& database_;
+    renderer::MapRenderer& renderer_;
+    router::TransportRouter& router_;
 };
 
-} // namespace transport_catalogue::query
+template<typename StopContainer>
+void Handler::AddBus(std::string name, const StopContainer& stop_names, Bus::RouteType route_type) {
+    database_.AddBus(std::move(name), stop_names, route_type);
+}
+
+template<typename From, typename Into>
+void Handler::ProcessQueries(From from, std::istream& input, Into into, std::ostream& output) {
+    into::ProcessQueries(ReadQueries(from, input), *this, into, output);
+}
+
+} // namespace transport_catalogue::queries
